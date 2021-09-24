ff716273e8c1d5338687cd61fec81b8e8bce711c
balachandar <govindalapudibalachandar@gmail.com>
2021-09-24 19:39:13 +0530

    Added:AddedNewContact file is added

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -2,17 +2,26 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     // main method
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System ! ");
-                Scanner sc = new Scanner(System.in);
-                AddressBook addressBook = new AddressBook();
-                addressBook.Add();
-                System.out.println("Thank for Using Address book");
-    }
-    }
+        //Create an Object
+        Contacts obj = new Contacts();
+        obj.setFirstName("Bala");
+        obj.setLastName("Chandar");
+        obj.setCity("MTM");
+        obj.setState("AP");
+        obj.setPin("521100");
+        obj.setMobileNo("9441520386");
+        obj.setEmail("Bala@gmail.com");
+        System.out.println("Contact details: "+obj.getFirstName()+" "+ obj.getLastName()+" "+ obj.getCity()+" "+ obj.getState()+" "+ obj.getPin()+" "+ obj.getMobileNo()+" "+ obj.getEmail());
+        System.out.println("Thank for Using Address book");
 
+    }
+}
 
 
 	
